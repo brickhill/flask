51db1f665150e2314f9c4f3a3cd25a2b10a02199
brickhill <petergibson@sbsys.co.uk>
2025-03-09 22:05:56 +0000

    DB added

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask, Blueprint
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 app = Flask(__name__)
-from app import routes
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
 
 from app.blog import bl as app_blog
 app.register_blueprint(app_blog, url_prefix='/blog')
-
--- app/blog/handlers.py
+++ app/blog/handlers.py
@@ -1,9 +1,18 @@
-from flask import render_template, request
+from flask import render_template, request, flash, redirect
 from app.blog import bl
 from app import app
+from .forms import LoginForm
 
 # @app.route('/index')
 @app.route('/blog/xxx')
 def blog_index():
     return render_template('blog/index.html')
 
+@app.route('/blog/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash("Login requested for user {} remember me = {}".
+        format(form.username.data, form.remember_me.data))
+        return redirect(url_for('blog_index'))
+    return render_template('blog/login.html', title="Sign in", form=form)
